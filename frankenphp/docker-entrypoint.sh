9e29df0a33fe96992f4d1303f5b391e201c896ba
Vincent Pasquier <vincentpasquier@posteo.net>
2026-07-26 00:45:39 +0200

    Adapt db init for .env.local usage

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -11,10 +11,10 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	# Or about an error in project initialization
 	php bin/console -V
 
-	if grep -q ^DATABASE_URL= .env; then
-		echo 'Waiting for database to be ready...'
+	if [ -n "${DATABASE_URL:-}" ]; then
+		echo 'Waiting for database server and creating database if needed...'
 		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
-		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
+		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console doctrine:database:create --if-not-exists --no-interaction 2>&1); do
 			if [ $? -eq 255 ]; then
 				# If the Doctrine command exits with 255, an unrecoverable error occurred
 				ATTEMPTS_LEFT_TO_REACH_DATABASE=0
@@ -22,15 +22,15 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 			fi
 			sleep 1
 			ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
-			echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
+			echo "Still waiting for database server... $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
 		done
 
 		if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
-			echo 'The database is not up or not reachable:'
+			echo 'The database server is not up or not reachable:'
 			echo "$DATABASE_ERROR"
 			exit 1
 		else
-			echo 'The database is now ready and reachable'
+			echo 'Database is ready'
 		fi
 
 		if [ "$(find ./migrations -iname '*.php' -print -quit)" ]; then
